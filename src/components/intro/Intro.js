62a5576af2b837302fb8f8d34e4942ea9c6b81b9
sebastianbuscaglia <sebastian.buscaglia@magneticash.com>
2024-03-11 14:11:19 -0300

    firts commit

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,52 @@
+import "./intro.css";
+
+import { FaAward } from "react-icons/fa";
+import React from "react";
+import { VscFolderLibrary } from "react-icons/vsc";
+import img from "../../assets/me.jpeg";
+
+const Intro = () => {
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={img} alt="Rasif Taghizade" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ year</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>5+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+            Competent Full-stack Developer with over a year of experience in building Web & Mobile applications.
+            <br />
+            My goal is to provide efficient and effective solutions, abiding by the following principles in my work:
+            <br />
+            - I focus on delivering solutions that cater to my client's needs and address their problems.
+            <br />
+            - I believe in keeping my clients engaged throughout the development process, ensuring they are fully informed and satisfied with the progress of their project.
+            <br />
+            Despite my relatively short tenure, I have demonstrated a fast acquisition of skills and the ability to deliver high-quality projects. I am excited by the opportunity to continue growing and tackling new challenges in the field of software development.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,73 @@
+import "./portfolio.css";
+
+import IMG1 from "../../assets/Lepark.png";
+import IMG2 from "../../assets/williams.png";
+import IMG3 from "../../assets/42payapp.png";
+import React from "react";
+
+//Portfolio function
+const Portfolio = () => {
+  const soloProjects = [
+    {
+      id: 1,
+      title: "App LePark",
+      img: IMG1,
+      description:
+        "LePark is the first app under the concept of 42payapp, which aims to replace traditional cards and tokens in entertainment venues with a user-friendly interface right at your fingertips.",
+      technologies: "React Native | Typescript | Python | Django | Angular | Ionic ",
+      link: "https://play.google.com/store/apps/details?id=com.lepark&hl=es_US&gl=US",
+    },
+    {
+      id: 2,
+      title: "Williams",
+      img: IMG2,
+      description:
+        "Williams was one of the first significant projects in my career, where the challenge was to create a web application that serves to manage sales agents and everything related to real estate. It is a completed and paid project.",
+      technologies: "Typescript | Tailwind | Firebase | React and Redux Toolkit",
+      link: "",
+    },
+    {
+      id: 3,
+      title: "42PayApp",
+      img: IMG3,
+      description:
+        "42PayApp is a digital platform, integrable with current (compatible) card systems, that offers entertainment center visitors the possibility to play with their mobile phones through an app without the need to purchase a physical card. (Work in progress)",
+      technologies: "Nginx | Angular | Typescript | Node.js",
+      link: "https://42payapp.com",
+    },
+  ];
+
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {soloProjects.map((pro) => (
+          <article className="portfolio__item" key={pro.id}>
+            <div className="portfolio__item-image">
+              <img src={pro.img} alt={pro.title} />
+            </div>
+            <div className="portfolio__item-content">
+              <h3>{pro.title}</h3>
+              <p>{pro.description}</p>
+              <p>{pro.technologies}</p>
+            </div>
+            <div className="portfolio__item-cta">
+              <a
+                href={pro.link}
+                target="_blank"
+                className="btn btn-primary"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
