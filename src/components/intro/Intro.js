a3452f17e48629ae8b03291c4074b8405e818369
Sebastianbuscaglia <sebastian.buscaglia@magneticash.com>
2024-08-21 18:41:51 -0300

    commit

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -8,7 +8,7 @@ import img from "../../assets/me.jpeg";
 const Intro = () => {
   return (
     <section id="about">
-      <h5>Get to know</h5>
+      <h5>Get to know</h5> 
       <h2>About Me</h2>
       <div className="container about__container">
         <div className="about__me">
